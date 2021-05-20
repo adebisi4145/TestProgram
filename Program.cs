04ef311ebf79a48a7ba84d610ed32f9dc37c060b
adebisi <adeyemiadebisi00@yahoo.com>
2021-05-20 12:00:15 +0100

    first code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("what is your name" );
+
+            string name = Console.ReadLine();
+
+            Console.Write("what is your favourite colour" );
+
+            string colour = Console.ReadLine();
+
+            string colou = Console.ReadLine();
+
+
+            Console.WriteLine($"Hello {name} {colour} ");
+            
+        }
+    }
+}
